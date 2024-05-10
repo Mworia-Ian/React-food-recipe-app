8b4f89683b3f4b97b089057583256f427a408920
adan <zizkoteam@gmail.com>
2024-05-10 20:29:24 +0300

    changes made

--- src/components/recipe-item/RecipeItem.jsx
+++ src/components/recipe-item/RecipeItem.jsx
@@ -14,7 +14,7 @@
 //   } = item;
 
 //   return (
-//     <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white ">
+//     <div className="flex flex-col w-80 overflow-hidden p-5 bg-orange-500 shadow-xl gap-5 border-2 rounded-2xl border-white ">
 //      <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
 //         <img
 //             src={image_url} alt="recipe item" className="block w-full"
@@ -51,16 +51,16 @@ function RecipeItem({ item }) {
   } = item;
 
   return (
-    <div className="flex flex-col w-80 overflow-hidden p-5 bg-orange-500 shadow-xl gap-5 border-2 rounded-2xl border-white ">
+    <div className="flex flex-col w-80 overflow-hidden p-5 bg-green-500 shadow-xl gap-5 border-2 rounded-2xl border-white ">
      <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
         <img
             src={image_url} alt="recipe item" className="block w-full"
          />
      </div>
      <div className="mt-4">
-      <span className="text-lg text-white font-bold"><b>Publisher:</b> {publisher}</span>
-      <h3 className="font-bold text-2xl truncate  text-white">{title}</h3>
-      <Link to={`./recipe-item/${recipe_id}`} className="text-sm mt-4 p-3 px-8 rounded-lg font-medium tracking-wider inline-block shadow-md bg-gradient-to-r from-blue-800 to-blue-700 text-orange uppercase hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600 hover:text-white">
+      <span className="text-lg text-black font-bold block"><b>Publisher:</b> {publisher}</span>
+      <h3 className="font-bold text-2xl truncate  text-black mt-2 ">{title}</h3>
+      <Link to={`./recipe-item/${recipe_id}`} className="text-sm mt-4 p-3 px-8 rounded-lg font-medium tracking-wider inline-block shadow-md bg-blue-800 text-orange uppercase hover:bg-white hover:text-orange hover:border-orange hover:border hover:border-orange"> 
       Recipe Details
       </Link>
      </div>
@@ -69,8 +69,6 @@ function RecipeItem({ item }) {
 }
 
 export default RecipeItem;
-
-
 
 
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,3 +1,29 @@
+// import { useContext } from "react";
+// import { GlobalContext } from "../../context/GlobalState";
+// import RecipeItem from "../../components/recipe-item/RecipeItem";
+
+// function Home() {
+//   const { recipeList, loading } = useContext(GlobalContext);
+
+//   if (loading) return <div>Please Wait as we fetch your recipes!!!</div>;
+
+//   return (
+//     <div className="py-8 container mx-auto bg-gray-300 flex flex-wrap justify-center gap-10">
+//       {recipeList && recipeList.length > 0 ? (
+//         recipeList.map((item) => <RecipeItem key={item.recipe_id} item={item} />)
+//       ) : (
+//         <div className="text-2xl font-bold">
+//           Search for your favorite recipe, I am sure we have it
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+import React from "react";
 import { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import RecipeItem from "../../components/recipe-item/RecipeItem";
@@ -8,7 +34,7 @@ function Home() {
   if (loading) return <div>Please Wait as we fetch your recipes!!!</div>;
 
   return (
-    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+    <div className="py-8 container mx-auto bg-gray-300 flex flex-wrap justify-center gap-10 bg-cover bg-center" style={{backgroundImage: "url('/src/home/image')"}}>
       {recipeList && recipeList.length > 0 ? (
         recipeList.map((item) => <RecipeItem key={item.recipe_id} item={item} />)
       ) : (
