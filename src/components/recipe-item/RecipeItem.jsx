0eb9b2e765012b20aca76047542e9b1691b8e2bf
adan <zizkoteam@gmail.com>
2024-05-10 10:37:34 +0300

    backgroun colour changed

--- src/components/recipe-item/RecipeItem.jsx
+++ src/components/recipe-item/RecipeItem.jsx
@@ -1,6 +1,43 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+
+// function RecipeItem({ item }) {
+//   const {
+//     publisher,
+//     title,
+//     // source_url,
+//     recipe_id,
+//     image_url,
+//     // social_rank,
+//     // publisher_url,
+//   } = item;
+
+//   return (
+//     <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white ">
+//      <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
+//         <img
+//             src={image_url} alt="recipe item" className="block w-full"
+//          />
+//      </div>
+//      <div>
+//       <span className="text-l text-orange-500 font-bold"><b>Publisher:</b> {publisher}</span>
+//       <h3 className="font-bold text-2xl truncate  text-orange-500">{title}</h3>
+//       <Link to={`./recipe-item/${recipe_id}`} className="text-sm mt-5 p-3 px-8 rounded-lg font-medium tracking-wider inline-block shadow-md bg-blue-950 text-orange uppercase ">
+//       Recipe Details
+//       </Link>
+//      </div>
+//     </div>
+//   );
+// }
+
+// export default RecipeItem;
+
+
+
+
 import React from "react";
 import { Link } from "react-router-dom";
-
 
 function RecipeItem({ item }) {
   const {
@@ -14,15 +51,15 @@ function RecipeItem({ item }) {
   } = item;
 
   return (
-    <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white">
+    <div className="flex flex-col w-80 overflow-hidden p-5 bg-orange-500 shadow-xl gap-5 border-2 rounded-2xl border-white ">
      <div className="h-40 flex justify-center overflow-hidden items-center rounded-xl">
         <img
             src={image_url} alt="recipe item" className="block w-full"
          />
      </div>
      <div>
-      <span className="text-l text-orange-500 font-bold"><b>Publisher:</b> {publisher}</span>
-      <h3 className="font-bold text-2xl truncate  text-orange-500">{title}</h3>
+      <span className="text-l text-white font-bold"><b>Publisher:</b> {publisher}</span>
+      <h3 className="font-bold text-2xl truncate  text-white">{title}</h3>
       <Link to={`./recipe-item/${recipe_id}`} className="text-sm mt-5 p-3 px-8 rounded-lg font-medium tracking-wider inline-block shadow-md bg-blue-950 text-orange uppercase ">
       Recipe Details
       </Link>
@@ -32,3 +69,8 @@ function RecipeItem({ item }) {
 }
 
 export default RecipeItem;
+
+
+
+
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js,jsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
