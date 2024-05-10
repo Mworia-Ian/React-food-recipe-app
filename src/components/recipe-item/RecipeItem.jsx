a6fd8b6a455b8a3c4fd7c85ea76f164ad4837bfb
adan <zizkoteam@gmail.com>
2024-05-10 11:17:00 +0300

    white space around the text has benn increased

--- src/components/recipe-item/RecipeItem.jsx
+++ src/components/recipe-item/RecipeItem.jsx
@@ -57,10 +57,10 @@ function RecipeItem({ item }) {
             src={image_url} alt="recipe item" className="block w-full"
          />
      </div>
-     <div>
-      <span className="text-l text-white font-bold"><b>Publisher:</b> {publisher}</span>
+     <div className="mt-4">
+      <span className="text-lg text-white font-bold"><b>Publisher:</b> {publisher}</span>
       <h3 className="font-bold text-2xl truncate  text-white">{title}</h3>
-      <Link to={`./recipe-item/${recipe_id}`} className="text-sm mt-5 p-3 px-8 rounded-lg font-medium tracking-wider inline-block shadow-md bg-gradient-to-r from-blue-800 to-blue-700 text-orange uppercase hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600 hover:text-white">
+      <Link to={`./recipe-item/${recipe_id}`} className="text-sm mt-4 p-3 px-8 rounded-lg font-medium tracking-wider inline-block shadow-md bg-gradient-to-r from-blue-800 to-blue-700 text-orange uppercase hover:bg-gradient-to-r hover:from-blue-700 hover:to-blue-600 hover:text-white">
       Recipe Details
       </Link>
      </div>
